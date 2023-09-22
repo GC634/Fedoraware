b45c8921c5663795bf06dc6e60fb75a637bc0e90
GC634 <110097220+GC634@users.noreply.github.com>
2023-09-22 21:07:47 +0200

    Hmm

--- Fedoraware/Fedoraware-TF2/src/Features/AntiHack/FakeLag/FakeLag.h
+++ Fedoraware/Fedoraware-TF2/src/Features/AntiHack/FakeLag/FakeLag.h
@@ -3,29 +3,29 @@
 
 class CFakeLag
 {
-	enum FakelagModes // Update this enum if you're adding/removing modes!
-	{
-		FL_Plain,
-		FL_Random,
-		FL_Adaptive
-	};
+    enum FakelagModes // Update this enum if you're adding/removing modes!
+    {
+        FL_Plain,
+        FL_Random,
+        FL_Adaptive
+    };
 
-	int ChokeCounter = 0; // How many ticks have been choked
-	int ChosenAmount = 0; // How many ticks should be choked
-	bool bPreservingBlast = false;
-	bool bUnducking = false;
-	bool bAttacked = false;
-	std::pair<bool, int> pInAirTicks = { false, 0 };	//	started on ground, ticks in air
-	Vec3 vLastPosition;
+    int ChokeCounter = 0; // How many ticks have been choked
+    int ChosenAmount = 0; // How many ticks should be choked
+    bool bPreservingBlast = false;
+    bool bUnducking = false;
+    bool bAttacked = false;
+    std::pair<bool, int> pInAirTicks = { false, 0 }; // Started on the ground, ticks in the air
+    Vec3 vLastPosition;
 
-	bool IsVisible(CBaseEntity* pLocal);
-	bool IsAllowed(CBaseEntity* pLocal);
-	void Prediction(const int nOldGroundInt, const int nOldFlags);	//	TODO: Create a struct for dealing with pre-prediction info.
-	void PreserveBlastJump(const int nOldGroundInt, const int nOldFlags);
-	void Unduck(const int nOldFlags);
+    bool IsVisible(CBaseEntity* pLocal);
+    bool IsAllowed(CBaseEntity* pLocal);
+    void Prediction(const int nOldGroundInt, const int nOldFlags); // TODO: Create a struct for dealing with pre-prediction info.
+    void PreserveBlastJump(const int nOldGroundInt, const int nOldFlags);
+    void Unduck(const int nOldFlags);
 
 public:
-	void OnTick(CUserCmd* pCmd, bool* pSendPacket, const int nOldGround, const int nOldFlags);
+    void OnTick(CUserCmd* pCmd, bool* pSendPacket, const int nOldGround, const int nOldFlags);
 };
 
 ADD_FEATURE(CFakeLag, FakeLag)
