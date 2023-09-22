b76284720be22fbfc096629c2e9b9de399ff145a
GC634 <110097220+GC634@users.noreply.github.com>
2023-09-22 21:06:29 +0200

    This was always so shit

--- Fedoraware/Fedoraware-TF2/src/Features/AutoQueue/AutoQueue.cpp
+++ Fedoraware/Fedoraware-TF2/src/Features/AutoQueue/AutoQueue.cpp
@@ -2,78 +2,78 @@
 
 void CAutoQueue::Run()
 {
-	const bool bInGame = (!I::EngineVGui->IsGameUIVisible() || I::EngineClient->IsInGame());
+    const bool bInGame = (!I::EngineVGui->IsGameUIVisible() || I::EngineClient->IsInGame());
 
-	// Auto queue
-	if (Vars::Misc::AutoCasualQueue.Value == 1)
-	{
-		if (bInGame)
-		{
-			return;
-		}
-		const bool bInStandbyQueue = I::TFPartyClient->BInStandbyQueue();
-		const bool bHaveLiveMatch = I::TFGCClientSystem->BHaveLiveMatch();
-		const int nNumMatchInvites = I::TFGCClientSystem->GetNumMatchInvites();
+    // Auto queue
+    if (Vars::Misc::AutoCasualQueue.Value == 1)
+    {
+        if (bInGame)
+        {
+            return;
+        }
 
-		if (!bInStandbyQueue &&
-			!bHaveLiveMatch &&
-			!nNumMatchInvites)
-		{
-			I::TFPartyClient->LoadSavedCasualCriteria();
-			I::TFPartyClient->RequestQueueForMatch(k_eTFMatchGroup_Casual_Default);
-		}
-	}
-	if (Vars::Misc::AutoCasualQueue.Value == 2)
-	{
-		const bool bInQueueForMatchGroup = I::TFPartyClient->BInQueueForMatchGroup(k_eTFMatchGroup_Casual_Default);
+        const bool bInStandbyQueue = I::TFPartyClient->BInStandbyQueue();
+        const bool bHaveLiveMatch = I::TFGCClientSystem->BHaveLiveMatch();
+        const int nNumMatchInvites = I::TFGCClientSystem->GetNumMatchInvites();
 
-		if (!bInQueueForMatchGroup)
-		{
-			I::TFPartyClient->LoadSavedCasualCriteria();
-			I::TFPartyClient->RequestQueueForMatch(k_eTFMatchGroup_Casual_Default);
-		}
-	}
+        if (!bInStandbyQueue &&
+            !bHaveLiveMatch &&
+            !nNumMatchInvites)
+        {
+            I::TFPartyClient->LoadSavedCasualCriteria();
+            I::TFPartyClient->RequestQueueForMatch(k_eTFMatchGroup_Casual_Default);
+        }
+    }
+    else if (Vars::Misc::AutoCasualQueue.Value == 2)
+    {
+        const bool bInQueueForMatchGroup = I::TFPartyClient->BInQueueForMatchGroup(k_eTFMatchGroup_Casual_Default);
 
-	if (bInGame)
-	{
-		return;
-	}
+        if (!bInQueueForMatchGroup)
+        {
+            I::TFPartyClient->LoadSavedCasualCriteria();
+            I::TFPartyClient->RequestQueueForMatch(k_eTFMatchGroup_Casual_Default);
+        }
+    }
 
-	// Auto accept
-	if (Vars::Misc::AntiVAC.Value)
-	{
-		static auto fps_max = g_ConVars.FindVar("fps_max");
-		static auto host_timescale = g_ConVars.FindVar("host_timescale");
-		static auto sv_cheats = g_ConVars.FindVar("sv_cheats");
-		static bool lastConnect = false;
+    if (bInGame)
+    {
+        return;
+    }
 
-		if (I::TFGCClientSystem->GetNumMatchInvites() > 0 && !I::EngineClient->IsConnected())
-		{
-			sv_cheats->SetValue(1);
-			fps_max->SetValue(1);
-			host_timescale->SetValue(25);
-		}
-		else if (I::TFGCClientSystem->BHaveLiveMatch() && !I::EngineClient->IsConnected() && lastConnect)
-		{
-			I::TFGCClientSystem->JoinMMMatch();
-		}
-		else if (I::EngineClient->IsConnected() && !lastConnect)
-		{
-			fps_max->SetValue(0);
-			host_timescale->SetValue(1);
-		}
+    // Auto accept
+    if (Vars::Misc::AntiVAC.Value)
+    {
+        static auto fps_max = g_ConVars.FindVar("fps_max");
+        static auto host_timescale = g_ConVars.FindVar("host_timescale");
+        static auto sv_cheats = g_ConVars.FindVar("sv_cheats");
+        static bool lastConnect = false;
 
-		lastConnect = I::EngineClient->IsConnected();
-	}
+        if (I::TFGCClientSystem->GetNumMatchInvites() > 0 && !I::EngineClient->IsConnected())
+        {
+            sv_cheats->SetValue(1);
+            fps_max->SetValue(1);
+            host_timescale->SetValue(25);
+        }
+        else if (I::TFGCClientSystem->BHaveLiveMatch() && !I::EngineClient->IsConnected() && lastConnect)
+        {
+            I::TFGCClientSystem->JoinMMMatch();
+        }
+        else if (I::EngineClient->IsConnected() && !lastConnect)
+        {
+            fps_max->SetValue(0);
+            host_timescale->SetValue(1);
+        }
 
-	// Join message spam
-	if (Vars::Misc::JoinSpam.Value)
-	{
-		static Timer spamTimer{ };
-		if (spamTimer.Run(200))
-		{
-			I::TFGCClientSystem->JoinMMMatch();
-		}
+        lastConnect = I::EngineClient->IsConnected();
+    }
 
-	}
+    // Join message spam
+    if (Vars::Misc::JoinSpam.Value)
+    {
+        static Timer spamTimer{ };
+        if (spamTimer.Run(200))
+        {
+            I::TFGCClientSystem->JoinMMMatch();
+        }
+    }
 }
