c6cdf57b39cb1f4f09a0909a65aba225682db7e4
GC634 <110097220+GC634@users.noreply.github.com>
2023-10-05 14:58:00 +0200

    Auto Queue is bad

--- Fedoraware/Fedoraware-TF2/src/Features/AutoQueue/AutoQueue.cpp
+++ Fedoraware/Fedoraware-TF2/src/Features/AutoQueue/AutoQueue.cpp
@@ -2,19 +2,24 @@
 
 void CAutoQueue::Run()
 {
+	// Check if the player is in game.
 	const bool bInGame = (!I::EngineVGui->IsGameUIVisible() || I::EngineClient->IsInGame());
 
-	// Auto queue
+	// Auto queue.
 	if (Vars::Misc::AutoCasualQueue.Value == 1)
 	{
+		// If the player is in game, return.
 		if (bInGame)
 		{
 			return;
 		}
+
+		// Check if the player is in standby queue, has a live match, or has any match invites.
 		const bool bInStandbyQueue = I::TFPartyClient->BInStandbyQueue();
 		const bool bHaveLiveMatch = I::TFGCClientSystem->BHaveLiveMatch();
 		const int nNumMatchInvites = I::TFGCClientSystem->GetNumMatchInvites();
 
+		// If the player is not in standby queue, does not have a live match, and does not have any match invites, queue for a casual match.
 		if (!bInStandbyQueue &&
 			!bHaveLiveMatch &&
 			!nNumMatchInvites)
@@ -23,10 +28,12 @@ void CAutoQueue::Run()
 			I::TFPartyClient->RequestQueueForMatch(k_eTFMatchGroup_Casual_Default);
 		}
 	}
-	if (Vars::Misc::AutoCasualQueue.Value == 2)
+	else if (Vars::Misc::AutoCasualQueue.Value == 2)
 	{
+		// Check if the player is in queue for a casual match.
 		const bool bInQueueForMatchGroup = I::TFPartyClient->BInQueueForMatchGroup(k_eTFMatchGroup_Casual_Default);
 
+		// If the player is not in queue for a casual match, queue for a casual match.
 		if (!bInQueueForMatchGroup)
 		{
 			I::TFPartyClient->LoadSavedCasualCriteria();
@@ -34,46 +41,50 @@ void CAutoQueue::Run()
 		}
 	}
 
-	if (bInGame)
-	{
-		return;
-	}
-
-	// Auto accept
+	// Auto accept.
 	if (Vars::Misc::AntiVAC.Value)
 	{
+		// Get the fps_max, host_timescale, and sv_cheats convars.
 		static auto fps_max = g_ConVars.FindVar("fps_max");
 		static auto host_timescale = g_ConVars.FindVar("host_timescale");
 		static auto sv_cheats = g_ConVars.FindVar("sv_cheats");
+
+		// Keep track of whether the player was connected last frame.
 		static bool lastConnect = false;
 
+		// If the player has match invites and is not connected to a server, set sv_cheats to 1, fps_max to 1, and host_timescale to 25.
 		if (I::TFGCClientSystem->GetNumMatchInvites() > 0 && !I::EngineClient->IsConnected())
 		{
 			sv_cheats->SetValue(1);
 			fps_max->SetValue(1);
 			host_timescale->SetValue(25);
 		}
+		// If the player has a live match and is not connected to a server, and was connected last frame, join the match.
 		else if (I::TFGCClientSystem->BHaveLiveMatch() && !I::EngineClient->IsConnected() && lastConnect)
 		{
 			I::TFGCClientSystem->JoinMMMatch();
 		}
+		// If the player is connected to a server and was not connected last frame, set fps_max to 0 and host_timescale to 1.
 		else if (I::EngineClient->IsConnected() && !lastConnect)
 		{
 			fps_max->SetValue(0);
 			host_timescale->SetValue(1);
 		}
 
+		// Update the value of lastConnect.
 		lastConnect = I::EngineClient->IsConnected();
 	}
 
-	// Join message spam
+	// Join message spam.
 	if (Vars::Misc::JoinSpam.Value)
 	{
+		// Create a timer to spam the join match command.
 		static Timer spamTimer{ };
+
+		// If the timer has expired, join the match.
 		if (spamTimer.Run(200))
 		{
 			I::TFGCClientSystem->JoinMMMatch();
 		}
-
 	}
 }
